43c6e127e447e6ded6441d7e7adbbb903d63a976
Zhang Zhao <zhangzzhao@qq.com>
2025-12-30 14:16:52 +0800

    张照

--- src/main/java/com/bigcomp/accesscontrol/Main.java
+++ src/main/java/com/bigcomp/accesscontrol/Main.java
@@ -7,6 +7,7 @@ import javax.swing.SwingUtilities;
 /**
  * Main program entry point
  */
+//zz张照
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
